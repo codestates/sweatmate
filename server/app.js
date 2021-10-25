04f86ff7e3076e7190d9ba44ada0f815d3d9df91
YUN-KC <wjdrlfgg@gmail.com>
2021-10-25 14:01:12 +0900

    [server] feather: #7 Set up file structure

--- server/app.js
+++ server/app.js
@@ -1,16 +1,45 @@
+const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
-const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const { sequelize } = require("./models");
 const config = require("./config");
+const {
+  port,
+  cors: { allowedOrigin },
+} = require("./config");
+
+const authRouter = require("./router/auth");
+const userRouter = require("./router/user");
+const gatheringRouter = require("./router/gathering");
+const notificationRouter = require("./router/notification");
+const chatRouter = require("./router/chat");
 
 const app = express();
 
+const corsOption = { origin: allowedOrigin, optionsSuccessStatus: 200, credentials: true };
+app.use(cors(corsOption));
 app.use(helmet());
 app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Let's Sweatmate!");
+});
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+app.use("/gathering", gatheringRouter);
+app.use("/chat", notificationRouter);
+app.use("/notification", chatRouter);
+
+app.use((req, res) => {
+  res.status(400).json({ message: "Invalid request" });
+});
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: `Something went wrong: ${err}` });
 });
 
 app.listen(config.port, async () => {
