f8375d1b9438173ac02d6d271e1423dd15d16477
Yunwoo Ji <unu12073@gmail.com>
2021-10-26 17:37:57 +0900

    [client] feat: #5 Implement Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,492 @@
-import React from "react";
+/* eslint-disable */
+
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import styled, { css } from "styled-components";
+import media from "styled-media-query";
+import {
+  IoNotificationsOutline,
+  IoChatbubblesOutline,
+  IoHomeOutline,
+  IoCalendarNumberOutline,
+  IoPowerOutline,
+} from "react-icons/io5";
+import { HiMenu } from "react-icons/hi";
+import { BsPerson } from "react-icons/bs";
+import UserThumbnail from "./UserThumbnail";
+
+const StyledHeader = styled.header`
+  background-color: var(--color-white);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  border-bottom: 1px solid var(--color-lightgray);
+  z-index: 10;
+
+  ${media.lessThan("medium")`
+    padding: 1rem;
+  `}
+`;
+
+const LogoLink = styled(Link)`
+  margin-right: 2rem;
+  height: 2rem;
+`;
+
+const LongLogo = styled.img`
+  height: 2rem;
+  display: block;
+
+  ${({ isLogin }) =>
+    !isLogin &&
+    css`
+      ${media.lessThan("medium")`
+        display: none;
+      `}
+    `};
+`;
+
+const ShortLogo = styled.img`
+  height: 2rem;
+  display: none;
+
+  ${({ isLogin }) =>
+    !isLogin &&
+    css`
+      ${media.lessThan("medium")`
+        display: block;
+      `}
+    `};
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex: 1;
+
+  ${media.lessThan("medium")`
+    display: ${({ isNav }) => (isNav ? "flex" : "none")};
+    flex-direction: column;
+    position: fixed;
+    top: 4rem;
+    left: 0;
+    height: 0;
+    width: 100vw;
+    height: 100vh;
+    padding: 1rem;
+    background-color: var(--color-white);
+    border-top: 1px solid var(--color-lightgray);
+  `}
+`;
+
+const MobileStyledH4 = styled.h4`
+  display: block;
+  padding-left: 1rem;
+  color: var(--color-gray);
+  margin-bottom: 1rem;
+
+  &:last-of-type {
+    margin-top: 2rem;
+    padding-top: 1rem;
+    border-top: 1px solid var(--color-lightgray);
+  }
+
+  ${media.greaterThan("medium")`
+    display: none;
+  `}
+`;
+
+const StyledNavLink = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  padding: 0.5rem;
+  margin-right: 0.5rem;
+  transition: background-color, color 100ms ease-out;
+
+  :hover {
+    color: var(--color-maingreen--100);
+  }
+
+  ${media.lessThan("medium")`
+    padding: 1rem;
+    font-size: 2rem;
+    margin-right: 0;
+    margin-bottom: 0.5rem;
+    border: none;
+  `}
+
+  &.active {
+    background-color: var(--color-maingreen--25);
+    color: var(--color-maingreen--100);
+  }
+`;
+
+const MobileStyledNavLink = styled(StyledNavLink)`
+  display: flex;
+
+  ${media.greaterThan("medium")`
+    display: none;
+  `}
+`;
+
+const Text = styled.span`
+  font-size: 1rem;
+  margin-left: 0.5rem;
+  line-height: 1;
+
+  ${media.lessThan("medium")`
+    margin-left: 1rem;
+    font-size: 1.2rem;
+  `}
+`;
+
+const MobileUserContainer = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 1rem;
+  background-color: #f5f5f5;
+  border-radius: 0.5rem;
+  font-size: 1.3rem;
+  align-items: center;
+  margin-bottom: 1rem;
+
+  ${media.greaterThan("medium")`
+      display: none;
+  `};
+`;
+
+const MobileNickname = styled.span`
+  margin-left: 1rem;
+`;
+
+const MobileNavBtn = styled.button`
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  font-size: 2rem;
+  margin-bottom: 0.5rem;
+  border-radius: 0.5rem;
+  color: var(--color-red);
+
+  ${media.greaterThan("medium")`
+    display: none;
+  `}
+`;
+
+const NonUserBtns = styled.div``;
+
+const NonUserBtn = styled.button`
+  font-size: 1rem;
+  padding: 0.75rem 1rem;
+  transition: background-color 100ms ease-out;
+  border-radius: 0.5rem;
+  margin-left: 0.5rem;
+  line-height: 1;
+  ${({ main }) =>
+    main &&
+    css`
+      color: var(--color-white);
+      background-color: var(--color-maingreen--100);
+    `}
+
+  :hover {
+    background-color: #f5f5f5;
+    ${({ main }) =>
+      main &&
+      css`
+        background-color: var(--color-maingreen--100);
+        opacity: 80%;
+      `}
+  }
+
+  ${media.lessThan("medium")`
+    padding: 0.5rem 0.75rem;
+    margin-left: 0.25rem;
+  `}
+`;
+
+const UserBtns = styled.div`
+  display: flex;
+  position: relative;
+`;
+
+const NotificationBtn = styled.button`
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  transition: background-color, 100ms, ease-out;
+
+  :hover {
+    background-color: #f5f5f5;
+  }
+
+  ${media.lessThan("medium")`
+    padding: 0;
+    :hover {
+      background-color: transparent;
+    }
+  `}
+`;
+
+const UserBtn = styled.button`
+  display: flex;
+  margin-left: 0.5rem;
+  align-items: center;
+  border-radius: 0.5rem;
+  padding: 0.5rem;
+  transition: background-color, 100ms, ease-out;
+
+  :hover {
+    background-color: #f5f5f5;
+  }
+
+  ${media.lessThan("medium")`
+    display: none;
+  `};
+`;
+
+const Nickname = styled.span`
+  margin-left: 0.5rem;
+`;
+
+const MobileHamburgerBtn = styled.button`
+  display: flex;
+  font-size: 1.5rem;
+  align-items: center;
+  margin-left: 0.5rem;
+
+  ${media.greaterThan("medium")`
+    display: none;
+  `};
+`;
+
+const PcUserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  bottom: calc(-4.5rem - 3px);
+  right: 0;
+  border-radius: 0.5rem;
+  border: 1px solid var(--color-lightgray);
+
+  ${media.lessThan("medium")`
+    display: none;
+  `};
+`;
+
+const PcUserInfoMyPageBtn = styled(Link)`
+  background-color: var(--color-white);
+  padding: 0.5rem 1rem;
+  font-size: 0.9rem;
+  border-radius: 0.5rem 0.5rem 0 0;
+  border-bottom: 1px solid var(--color-lightgray);
+  transition: background-color, 100ms, ease-out;
+
+  :hover {
+    background-color: #f5f5f5;
+  }
+`;
+
+const PcUserInfoLogoutBtn = styled.button`
+  color: var(--color-red);
+  padding: 0.5rem 1rem;
+  font-size: 0.9rem;
+  border-radius: 0 0 0.5rem 0.5rem;
+  transition: background-color, opacity 100ms, ease-out;
+
+  :hover {
+    background-color: var(--color-red);
+    opacity: 25%;
+  }
+`;
+
+const NotificationContainer = styled.div`
+  position: relative;
+`;
+
+const NotificationWrapper = styled.div`
+  position: absolute;
+  top: 3rem;
+  right: 0;
+  border: 1px solid var(--color-lightgray);
+  border-radius: 0.5rem;
+  display: flex;
+  flex-direction: column;
+
+  ${media.lessThan("medium")`
+    top: 2.25rem;
+  `};
+`;
+
+const NotificationItem = styled.button`
+  background-color: var(--color-white);
+  padding: 0.5rem 1rem;
+  font-size: 0.9rem;
+  width: 20rem;
+
+  :not(:last-child) {
+    border-bottom: 1px solid var(--color-lightgray);
+  }
+
+  :first-child {
+    border-radius: 0.5rem 0.5rem 0 0;
+  }
+
+  :last-child {
+    border-radius: 0 0 0.5rem 0.5rem;
+  }
+
+  ${media.lessThan("medium")`
+    width: 16rem;
+  `};
+`;
 
 const Header = () => {
-  return <div>Header</div>;
+  const [isHamburgerBtnClicked, setIsHamburgerBtnClicked] = useState(false);
+  const [isUserBtnClicked, setIsUserBtnClicked] = useState(false);
+  const [isNotiBtnClicked, setIsNotiBtnClicked] = useState(false);
+  const isLogin = true;
+
+  const handleHamburgerClick = () => {
+    setIsHamburgerBtnClicked((prev) => !prev);
+    setIsNotiBtnClicked(false);
+    setIsUserBtnClicked(false);
+  };
+
+  const HandleUserInfoClick = () => {
+    setIsUserBtnClicked((prev) => !prev);
+    setIsNotiBtnClicked(false);
+    setIsHamburgerBtnClicked(false);
+  };
+
+  const HandleNotiClick = () => {
+    setIsNotiBtnClicked((prev) => !prev);
+    setIsUserBtnClicked(false);
+    setIsHamburgerBtnClicked(false);
+  };
+
+  const closeAll = () => {
+    setIsHamburgerBtnClicked(false);
+    setIsNotiBtnClicked(false);
+    setIsUserBtnClicked(false);
+  };
+
+  // TODO: Nickname
+  // TODO: 마이페이지 버튼 링크
+  // TODO: Logout btn 기능 구현
+
+  return (
+    <StyledHeader>
+      <LogoLink to="/" onClick={closeAll}>
+        <LongLogo
+          src={`${process.env.PUBLIC_URL}/assets/long-logo.png`}
+          alt="long-logo"
+          isLogin={isLogin}
+        />
+        <ShortLogo
+          src={`${process.env.PUBLIC_URL}/assets/short-logo.png`}
+          alt="short-logo"
+          isLogin={isLogin}
+        />
+      </LogoLink>
+      {isLogin && (
+        <Nav isNav={isHamburgerBtnClicked}>
+          <MobileUserContainer>
+            <UserThumbnail
+              size={2}
+              user={{
+                id: "uuid",
+                nickname: "농구에 미친 사람",
+                image: "",
+              }}
+            />
+            <MobileNickname>Unuuuuu</MobileNickname>
+          </MobileUserContainer>
+          <MobileStyledH4>Page</MobileStyledH4>
+          <StyledNavLink to="/home" onClick={closeAll}>
+            <IoHomeOutline />
+            <Text>홈</Text>
+          </StyledNavLink>
+          <StyledNavLink to="/chat/1" onClick={closeAll}>
+            <IoChatbubblesOutline />
+            <Text>채팅</Text>
+          </StyledNavLink>
+          <StyledNavLink to="/schedule" onClick={closeAll}>
+            <IoCalendarNumberOutline />
+            <Text>일정</Text>
+          </StyledNavLink>
+          <MobileStyledH4>Account</MobileStyledH4>
+          <MobileStyledNavLink to="/users/1" onClick={closeAll}>
+            <BsPerson />
+            <Text>마이 페이지</Text>
+          </MobileStyledNavLink>
+          <MobileNavBtn>
+            <IoPowerOutline />
+            <Text>로그아웃</Text>
+          </MobileNavBtn>
+        </Nav>
+      )}
+      {!isLogin && (
+        <NonUserBtns>
+          <NonUserBtn>게스트 로그인</NonUserBtn>
+          <NonUserBtn>로그인</NonUserBtn>
+          <NonUserBtn main>회원가입</NonUserBtn>
+        </NonUserBtns>
+      )}
+      {isLogin && (
+        <UserBtns>
+          <NotificationContainer>
+            <NotificationBtn onClick={HandleNotiClick}>
+              <IoNotificationsOutline />
+            </NotificationBtn>
+            {isNotiBtnClicked && (
+              <NotificationWrapper>
+                <NotificationItem>
+                  반갑습니다!
+                  <br /> 스웻 메이트에서 운동 친구를 만나 보세요.
+                  <br /> 관심있는 운동 종목을 설정하면,
+                  <br /> 스웻 메이트에서 운동 친구를 찾기 더 수월합니다.
+                </NotificationItem>
+                <NotificationItem>무슨 모임에서 새로운 채팅이 있습니다.</NotificationItem>
+                <NotificationItem>무슨 모임의 약속 시간이 하루 남았습니다.</NotificationItem>
+                <NotificationItem>무슨 모임에서 강퇴당했습니다.</NotificationItem>
+                <NotificationItem>무슨 모임이 종료되었습니다.</NotificationItem>
+              </NotificationWrapper>
+            )}
+          </NotificationContainer>
+          <UserBtn onClick={HandleUserInfoClick}>
+            <UserThumbnail
+              size={1.5}
+              user={{
+                id: "uuid",
+                nickname: "농구에 미친 사람",
+                image: "",
+              }}
+            />
+            <Nickname>Unuuuuu</Nickname>
+          </UserBtn>
+          <MobileHamburgerBtn onClick={handleHamburgerClick}>
+            <HiMenu />
+          </MobileHamburgerBtn>
+          {isUserBtnClicked && (
+            <PcUserInfo>
+              <PcUserInfoMyPageBtn to="/users/1" onClick={HandleUserInfoClick}>
+                마이 페이지
+              </PcUserInfoMyPageBtn>
+              <PcUserInfoLogoutBtn>로그아웃</PcUserInfoLogoutBtn>
+            </PcUserInfo>
+          )}
+        </UserBtns>
+      )}
+    </StyledHeader>
+  );
 };
 
 export default Header;
