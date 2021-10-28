fc0b3364aa09fa1c36977f082bdfe0d4681b33fb
ssumniee <suemink.dev@gmail.com>
2021-10-28 19:52:12 +0900

    [client] feat: #76 Add gathering detail modal status to redux store

--- client/src/store/actions/actionTypes.js
+++ client/src/store/actions/actionTypes.js
@@ -1,4 +1,5 @@
 // Modal
 export const GATH_CREATE_MODAL_ON = "GATH_CREATE_MODAL_ON";
+export const GATH_DETAIL_MODAL_ON = "GATH_DETAIL_MODAL_ON";
 export const CONFIRM_MODAL_ON = "CONFIRM_MODAL_ON";
 export const MODAL_OFF = "MODAL_OFF";
