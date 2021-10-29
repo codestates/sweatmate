ea84e9c7a3c5de53e9be3ec6ef6faadb6caa7dd6
ssumniee <suemink.dev@gmail.com>
2021-10-29 13:02:47 +0900

    [client] feat: #112 Update AnimaLogo component's responsive CSS

--- client/src/components/AnimaLogo.jsx
+++ client/src/components/AnimaLogo.jsx
@@ -9,36 +9,73 @@ import media from "styled-media-query";
     위 속성이 존재하지 않는 경우, 부모 컴포넌트의 100%를 차지합니다.
 */
 
-const LogoContainer = styled.div`
-  ${(props) => {
-    if (props.isFullscreen) return "position: absolute;\ntop: 0;\nleft: 0;";
-    else return "position: relative;";
-  }}
+const LogoWrapper = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
+  ${(props) =>
+    props.isFullscreen &&
+    `position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;`}
+  padding-top: 7.5%;
+  /* overflow: auto; */
+  outline: 0;
+`;
+
+const LogoContainer = styled.div`
+  position: relative;
+  width: fit-content;
+  height: 3rem;
+  left: 50%;
+  top: 50%;
+  margin: 4.5rem 0 0.75rem 0;
+  transform: translate(-50%, -50%);
+  ${media.between("small", "medium")`
+    height: 2.625rem;
+    margin: 3.938rem 0 0.656rem 0;
+  `}
+  ${media.lessThan("small")`
+    height: 2rem;
+    margin: 3rem 0 0.5rem 0;
+  `}
   display: flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
+  > * {
+    height: 100%;
+  }
 `;
 
 const InnerDiv = styled.div`
-  background-size: cover;
-  height: calc(4rem * 0.75);
+  background-size: contain;
+  background-repeat: no-repeat;
 `;
 
 const Lets = styled(InnerDiv)`
   background-image: url("../animation_assets/Lets.png");
   width: calc(3rem * 2.025);
   margin-right: 3rem;
+  ${media.between("small", "medium")`
+    width: calc(2.625rem * 2.025);
+    margin-right: 2.625rem;
+  `}
   ${media.lessThan("small")`
-    margin-right: 2.4rem;
+    width: calc(2rem * 2.025);
+    margin-right: 2rem;
   `}
 `;
 
 const AnimaContainer = styled(InnerDiv)`
-  width: calc(3rem * (5.7 + 1 + 0.542));
+  width: calc(3rem * (5.7 + 0.542 - 0.25));
+  ${media.between("small", "medium")`
+    width: calc(2.625rem * (5.7 + 0.542 - 0.25));
+  `}
   ${media.lessThan("small")`
-    width: calc(2.4rem * (5.7 + 1 + 0.542));
+    width: calc(2rem * (5.7 + 0.542 - 0.25));
   `}
   display: flex;
   align-items: baseline;
@@ -118,16 +155,19 @@ const IconDisappear = keyframes`
 `;
 
 const Icon = styled.div`
-  width: 3rem;
-  height: 3rem;
   position: absolute;
+  left: -1.5rem;
   bottom: 0;
-  left: calc(-0.5 * 3rem);
   background-size: cover;
+  width: 3rem;
+  height: 100%;
+  ${media.between("small", "medium")`
+    left: -1.3125rem;
+    width: 2.625rem;
+  `}
   ${media.lessThan("small")`
-    width: 2.4rem;
-    height: 2.4rem;
-    left: calc(-0.5 * 2.4rem);
+    left: -1rem;
+    width: 2rem;
   `}
   animation: ${Bounce} 0.6s alternate 10 cubic-bezier(0.75, 0.1, 0.04, 0.8) forwards,
     ${LastBounce} 1.2s 6s alternate 1 cubic-bezier(0.4, 0.1, 0.02, 0.8) forwards,
@@ -139,35 +179,36 @@ const LogoResize = keyframes`
 0% {
   width: 0rem;
 }
-100% {
-  width: calc(3rem * 5.7);
-}
 `;
 
 const Logo = styled(Icon)`
-  width: 0rem;
+  width: calc(3rem * 5.7);
+  ${media.between("small", "medium")`
+    width: calc(2.625rem * 5.7);
+  `}
   ${media.lessThan("small")`
-    width: calc(2.4rem * 5.7);
+    width: calc(2rem * 5.7);
   `}
   background-image: url("../animation_assets/Sweatmate.png");
-  animation: ${LogoResize} 0.9s 6.9s normal 1 cubic-bezier(0.75, 0.1, 0.04, 0.8) forwards;
+  animation: ${LogoResize} 0.9s 6.9s normal 1 cubic-bezier(0.75, 0.1, 0.04, 0.8) backwards;
 `;
 
 const MarkPosition = keyframes`
 100% {
-  left: calc(3rem * (5.7 - 0.25));
-  ${media.lessThan("small")`
-    left: calc(2.4rem * (5.7 - 0.25));
-  `}
+  transform: translateX(867.692%);
 }
 `;
 
 const ExcMark = styled(Icon)`
   width: 1.625rem;
   left: calc(3rem * (1 - 0.25));
+  ${media.between("small", "medium")`
+    width: 1.422rem;
+    left: calc(2.625rem * (1 - 0.25));
+  `}
   ${media.lessThan("small")`
-    width: 1.3rem;
-    left: calc(2.4rem * (1 - 0.25));
+    width: 1.083rem;
+    left: calc(2rem * (1 - 0.25));
   `}
   background-image: url("../animation_assets/ExcMark.png");
   animation: ${MarkPosition} 0.9s 6.9s normal 1 cubic-bezier(0.75, 0.1, 0.04, 0.8) forwards;
@@ -193,25 +234,32 @@ const Shadow = styled.div`
   width: 2.4rem;
   height: 0.48rem;
   border-radius: 100%;
+  ${media.between("small", "medium")`
+    width: 2.1rem;
+    height: 0.42rem;
+    left: calc(-0.5 * 2.1rem);
+  `}
   ${media.lessThan("small")`
-    width: 2rem;
-    height: 0.4rem;
-    left: calc(-0.5 * 2rem);
-  `};
+    width: 1.6rem;
+    height: 0.32rem;
+    left: calc(-0.5 * 1.6rem);
+  `}
   animation: ${ShadowBounce} 0.6s alternate 8 cubic-bezier(0.75, 0.1, 0.04, 0.8);
 `;
 
 const AnimaLogo = ({ isFullscreen }) => {
   return (
-    <LogoContainer isFullscreen={isFullscreen}>
-      <Lets />
-      <AnimaContainer>
-        <Shadow />
-        <Icon />
-        <Logo />
-        <ExcMark />
-      </AnimaContainer>
-    </LogoContainer>
+    <LogoWrapper isFullscreen={isFullscreen}>
+      <LogoContainer>
+        <Lets />
+        <AnimaContainer>
+          <Shadow />
+          <Icon />
+          <Logo />
+          <ExcMark />
+        </AnimaContainer>
+      </LogoContainer>
+    </LogoWrapper>
   );
 };
 
