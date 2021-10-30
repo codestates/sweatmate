23e6be53a8833347c2a8c6d0080dfb924d9652c8
YUN-KC <wjdrlfgg@gmail.com>
2021-10-30 13:55:01 +0900

    [server] feat: #127 Add chat router

--- server/router/chat.js
+++ server/router/chat.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// router.get("/", () => {});
-// router.post("/", () => {});
-// router.patch("/", () => {});
-// router.put("/", () => {});
-// router.delete("/", () => {});
-router.use((_, res) => {
-  res.status(200).send("준비중입니다.");
-});
+router.get("/:gatheringId", () => {}); // 채팅방 입장 라우터
+router.post("/:gatheringId", () => {}); // 채팅 입력 라우터
+router.delete("/:gatheringId/:userId", () => {}); // 호스트의 유저 추방 라우터
 
 module.exports = router;
