10fe010bebee65665bedf837ead4144c5bc0a0e4
ssumniee <suemink.dev@gmail.com>
2021-11-02 11:06:25 +0900

    [client] feat: #103 Update findGath api

--- client/src/api/gath.js
+++ client/src/api/gath.js
@@ -3,7 +3,13 @@ import api from "./index";
 const gathApi = {
   getSportList: () => api.get("/sportList.json"),
   getAreaList: () => api.get("/areaList.json"),
-  findGath: () => api.get("/gathering/"),
+  findGath: (queries) => {
+    let query = `sportName=${queries.sport}&areaName=${queries.area}`;
+    if (queries.date) query += `&date=${queries.date}`;
+    if (queries.time) query += `&time=${queries.time}`;
+    if (queries.totalNum) query += `&totalNum=${queries.totalNum}`;
+    return api.get(`/gathering?${query}`);
+  },
   createGath: () => api.post("/gathering"),
   joinGath: (gatheringId) => api.post(`/gathering/${gatheringId}`),
   endGath: (gatheringId) => api.patch(`/gathering/${gatheringId}`),
