0219710bc68d63dc1fbed35e0810083f2a3a15b5
YUN-KC <wjdrlfgg@gmail.com>
2021-11-04 00:28:26 +0900

    [server] chore: Modify MongoDB schema about chat

--- server/schemas/chat.js
+++ server/schemas/chat.js
@@ -1,5 +1,21 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const chatLog = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+  },
+});
 const chatSchema = new Schema({
   _id: {
     type: Number,
@@ -9,14 +25,14 @@ const chatSchema = new Schema({
     type: Object, // 타이틀, 스포츠 (한글), 스포츠 이모지,
   },
   chatLog: {
-    type: Array,
+    type: [chatLog],
     required: true,
     // default: [{ user: "userid", message: "새로운 모임이 만들어졌습니다.", date: Date() },] //{id, userId, nickname, image, date}
   },
   creatorId: {
     type: String,
     required: true,
-  }, // 유저 회원탈퇴 시 같이 삭제
+  },
 });
 
 module.exports = mongoose.model("Chat", chatSchema);
