85f8215ed20a4da1eac6bc2e674aefef385a8c5f
YUN-KC <wjdrlfgg@gmail.com>
2021-11-04 17:17:58 +0900

    [server] feat: #191 Add mongoDB notification schema

--- server/schemas/notification.js
+++ server/schemas/notification.js
@@ -1,6 +1,24 @@
 const mongoose = require("mongoose");
-
+// type = ["welcome", "new", "ban", "join", "leave", "done", "notice"]
 const { Schema } = mongoose;
+const notificationListSchema = new Schema({
+  id: {
+    type: Number,
+    default: null,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    default: null,
+  },
+  target: {
+    type: String,
+    default: null,
+  },
+});
 const notificationSchema = new Schema({
   // mysql 유저의 uuid 값을 넣어줌
   _id: {
@@ -8,9 +26,7 @@ const notificationSchema = new Schema({
     required: true,
   },
   // 알림목록들
-  notification: {
-    type: Array,
-  },
+  notification: [notificationListSchema],
 });
 
 module.exports = mongoose.model("notification", notificationSchema);
--- server/socket.js
+++ server/socket.js
@@ -1,20 +1,45 @@
 const SocketIO = require("socket.io");
+const cookie = require("cookie");
 const {
   cors: { allowedOrigin },
 } = require("./config");
+const { verifyAccessToken } = require("./controllers/functions/token");
+
 module.exports = (server, app) => {
   const io = SocketIO(server, {
     path: "/socket.io",
     cors: {
       origin: allowedOrigin,
+      credentials: true,
     },
-    serveClient: false, //TODO: 클라이언트에서 socket 설치하면 false 로 바꿔주기
+    serveClient: false, // TODO: 클라이언트에서 socket 설치하면 false 로 바꿔주기
   });
+
+  io.use(async (socket, next) => {
+    const { jwt } = cookie.parse(socket.handshake.headers.cookie || "");
+    if (!jwt) {
+      return next(new Error("Authentication error"));
+    }
+    const decoded = await verifyAccessToken(jwt);
+    if (!decoded) {
+      return next(new Error("Authentication error"));
+    }
+    next();
+  });
+
   app.set("io", io);
+
   io.on("connection", (socket) => {
-    console.log("새로운 클라이언트 접속!");
+    console.log("🔥 새로운 클라이언트 접속!", socket.id);
     socket.on("disconnect", () => {
-      console.log(socket.id, " 접속 해제");
+      console.log("❌ 클라이언트 접속 해제!", socket.id);
+    });
+  });
+  const chatNamespace = io.of("/chat");
+  chatNamespace.on("connection", (socket) => {
+    console.log("🔥 /chat 새로운 클라이언트 접속!", socket.id);
+    socket.on("disconnect", () => {
+      console.log("❌ /chat 클라이언트 접속 해제!", socket.id);
     });
   });
 };
