dc7893263ba9ca4dab9cadb61944609dc09fb6f7
ssumniee <suemink.dev@gmail.com>
2021-11-04 18:24:24 +0900

    [client] feat: #186 Separate search bar component from Home & Create HomeSearchBar

---
+++ client/src/components/HomeSearchBar.jsx
@@ -0,0 +1,234 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import media from "styled-media-query";
+import { IoSearch } from "react-icons/io5";
+import SearchInput from "./SearchInput";
+import InputDatepicker from "./InputDatepicker";
+import InputDatalist from "./InputDatalist";
+import InputTotalNum from "./InputTotalNum";
+import { useDispatch } from "react-redux";
+import gathApi from "../api/gath";
+import { searchModalOnAction, searchGathAction } from "../store/actions";
+
+const InputContainer = styled.form`
+  margin-bottom: 2rem;
+  height: 4rem;
+  background-color: var(--color-white);
+  border-radius: 1rem;
+  display: flex;
+  ${media.lessThan("medium")`
+  margin-bottom: 1.25rem;
+  width: calc(100% - 6rem);
+  `}
+  ${media.lessThan("small")`
+  width: 100%;
+  min-width: 20rem;
+  `}
+`;
+
+const Placeholder = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0.5rem;
+  position: relative;
+  #placeholder-icon {
+    flex: 0 0 auto;
+    color: var(--color-maingreen--100);
+  }
+  #placeholder-text {
+    flex: 1 1 auto;
+    line-height: 3rem;
+    color: var(--color-gray);
+    font-family: Interop-Light;
+    font-size: 1.25rem;
+    display: inline;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+`;
+
+const InputList = styled.div`
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  display: flex;
+`;
+
+const SearchIcon = styled(IoSearch)`
+  width: ${(prop) => prop.size}rem;
+  height: ${(prop) => prop.size}rem;
+  padding: ${(prop) => prop.size * 0.25}rem;
+  display: inline-block;
+`;
+
+const SearchBtnView = styled(SearchIcon)`
+  background-color: var(--color-maingreen--75);
+  color: var(--color-white);
+  border-radius: 0.6rem;
+  cursor: not-allowed;
+  ${(props) =>
+    props.disabled &&
+    `opacity : 0.5;
+    cursor: not-allowed;`}
+`;
+
+const SearchBtnContainer = styled.div`
+  flex: 0 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  position: relative;
+`;
+
+const SubmitInput = styled.input`
+  position: absolute;
+  left: 0.5rem;
+  top: 0.5rem;
+  right: 0.5rem;
+  bottom: 0.5rem;
+  width: calc(100% - 1rem);
+  border-radius: 0.6rem;
+  z-index: 1;
+  :hover {
+    cursor: pointer;
+    background-color: var(--color-white);
+    opacity: 0.2;
+  }
+`;
+
+const HomeSearchBar = () => {
+  const dispatch = useDispatch();
+  const [sportInput, setSportInput] = useState("");
+  const [areaInput, setAreaInput] = useState("");
+  const [dateInput, setDateInput] = useState("");
+  const [timeInput, setTimeInput] = useState("");
+  const [totalNumInput, setTotalNumInput] = useState(null);
+  const [searchable, setSearchable] = useState(false);
+  const [list, setList] = useState({
+    sport: [],
+    area: [],
+    time: [
+      { id: 1, timeName: "오전" },
+      { id: 2, timeName: "오후" },
+      { id: 3, timeName: "저녁" },
+    ],
+  });
+
+  useEffect(() => {
+    // 운동, 지역 리스트 받아오기
+    const getList = async () => {
+      try {
+        const sportList = await gathApi.getSportList();
+        const areaList = await gathApi.getAreaList();
+        setList({ ...list, sport: sportList.data, area: areaList.data });
+      } catch (err) {
+        // console.error(err);
+      }
+    };
+    getList();
+    // 모임 전체 리스트 받아오기
+    const getAllGathering = async () => {
+      try {
+        const res = await gathApi.getAllGath();
+        // 리덕스 스토어의 검색 조건, 모임 리스트 업데이트
+        dispatch(searchGathAction(res.data));
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getAllGathering();
+  }, []);
+
+  useEffect(() => {
+    if (sportInput && areaInput) {
+      setSearchable(true);
+    } else {
+      setSearchable(false);
+    }
+  }, [sportInput, areaInput]);
+
+  const handleSearchModalOn = () => {
+    dispatch(searchModalOnAction);
+  };
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      // 검색 조건 정제
+      const refinedSportInput = sportInput.match(/[A-Za-z가-힣]*/).join("");
+      const refinedDateInput = dateInput
+        .match(/[0-9]*/g)
+        .filter((el) => el.length > 0)
+        .join("-");
+      // 모임 검색
+      const res = await gathApi.findGath({
+        sport: refinedSportInput,
+        area: areaInput,
+        date: refinedDateInput,
+        time: timeInput,
+        totalNum: totalNumInput,
+      });
+      // 리덕스 스토어의 검색 조건, 모임 리스트 업데이트
+      dispatch(searchGathAction(res.data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <InputContainer onSubmit={handleSubmit}>
+      <InputList className="pc">
+        <SearchInput isSport name="운동" for="sport">
+          <InputDatalist
+            id="sport"
+            values={list.sport}
+            placeholder="어떤 운동하세요?"
+            item={sportInput}
+            setItem={setSportInput}
+          />
+        </SearchInput>
+        <SearchInput name="지역" for="area">
+          <InputDatalist
+            id="area"
+            values={list.area}
+            placeholder="지역 입력"
+            item={areaInput}
+            setItem={setAreaInput}
+          />
+        </SearchInput>
+        <SearchInput isDate name="날짜" for="date">
+          <InputDatepicker id="date" placeholder="날짜 입력" setDisplayedDate={setDateInput} />
+        </SearchInput>
+        <SearchInput isTime name="시간" for="time">
+          <InputDatalist
+            id="time"
+            values={list.time}
+            placeholder="시간 입력"
+            item={timeInput}
+            setItem={setTimeInput}
+          />
+        </SearchInput>
+        <SearchInput name="인원" for="totalNum">
+          <InputTotalNum
+            inputId="totalNum"
+            placeholder="인원 입력"
+            total={totalNumInput}
+            setTotal={setTotalNumInput}
+          />
+        </SearchInput>
+      </InputList>
+      <SearchBtnContainer className="pc">
+        {searchable && <SubmitInput type="submit" value="" />}
+        <SearchBtnView size={3} className="search-gathering" disabled={!searchable} />
+      </SearchBtnContainer>
+      <Placeholder className="mobile" onClick={handleSearchModalOn}>
+        <SearchIcon id="placeholder-icon" size={3} />
+        <p id="placeholder-text">어떤 운동하세요?</p>
+      </Placeholder>
+    </InputContainer>
+  );
+};
+
+export default HomeSearchBar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import GathCard from "../components/GathCard";
 import media from "styled-media-query";
 import OnMapBtn from "../components/OnMapBtn";
 import Btn from "../components/Btn";
-import { IoSearch } from "react-icons/io5";
-import SearchInput from "../components/SearchInput";
-import InputDatepicker from "../components/InputDatepicker";
-import InputDatalist from "../components/InputDatalist";
-import InputTotalNum from "../components/InputTotalNum";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import authApi from "../api/auth";
-import gathApi from "../api/gath";
-import {
-  gathCreateModalOnAction,
-  searchAction,
-  signinAction,
-  signoutAction,
-} from "../store/actions";
+import { gathCreateModalOnAction, signinAction, signoutAction } from "../store/actions";
+import HomeSearchBar from "../components/HomeSearchBar";
 
 const HomeContainer = styled.div`
   width: 100%;
@@ -31,85 +21,59 @@ const HomeContainer = styled.div`
       padding: 2rem 1rem;
     `}
   }
+  .pc {
+    ${media.lessThan("medium")`
+      display: none;
+    `}
+  }
+  .mobile {
+    display: none;
+    ${media.lessThan("medium")`
+      display: block;
+    `}
+  }
 `;
 
 const SearchContainer = styled.div`
   background-color: var(--color-maingreen--25);
   min-height: 20rem;
   flex: 0 0 1;
-  padding: 2rem;
+  padding: 4rem 2rem;
+  ${media.lessThan("medium")`
+    padding : 2rem 1rem;
+  `}
   display: flex;
   flex-direction: column;
   align-items: center;
   .create-gathering {
     width: 16rem;
+    ${media.lessThan("medium")`
+      margin-bottom: 1.25rem;
+    `}
+    ${media.lessThan("small")`
+      width: 100%;
+      min-width: 20rem;
+      height: 3rem;
+    `}
     background-color: var(--color-maingreen--75);
     color: var(--color-white);
-    margin-bottom: 4rem;
   }
 `;
 
 const SearchTitle = styled.h1`
   font-family: Interop-Regular;
   font-size: 1.25rem;
+  line-height: var(--lineHeight-normal);
   color: var(--color-darkgray);
-  line-height: var(--lineHeight-loose);
-  margin: 4rem 0 2rem;
-`;
-
-const InputContainer = styled.form`
-  margin: 0 auto;
-  height: 4rem;
-  background-color: var(--color-white);
-  border-radius: 1rem;
-  display: flex;
-`;
-
-const InputList = styled.div`
-  display: flex;
-  align-items: center;
-  display: flex;
-`;
-
-const SearchBtnView = styled(IoSearch)`
-  background-color: var(--color-maingreen--75);
-  color: var(--color-white);
-  padding: 0.75rem;
-  width: 3rem;
-  height: 3rem;
-  font-size: 1.5rem;
   text-align: center;
-  border-radius: 0.6rem;
-  cursor: not-allowed;
-  ${(props) =>
-    props.disabled &&
-    `opacity : 0.5;
-    cursor: not-allowed;`}
-`;
-
-const SearchBtnContainer = styled.div`
-  flex: 0 0 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0.5rem;
-  position: relative;
-`;
-
-const SubmitInput = styled.input`
-  position: absolute;
-  left: 0.5rem;
-  top: 0.5rem;
-  right: 0.5rem;
-  bottom: 0.5rem;
-  width: calc(100% - 1rem);
-  border-radius: 0.6rem;
-  z-index: 1;
-  :hover {
-    cursor: pointer;
-    background-color: var(--color-white);
-    opacity: 0.2;
-  }
+  margin: 2rem auto;
+  ${media.lessThan("medium")`
+    margin: 1.25rem auto;
+    font-size: 1.125rem;
+  `}
+  ${media.lessThan("small")`
+    font-size: 1.125rem;
+  `}
 `;
 
 const ListContainer = styled.div`
@@ -138,6 +102,7 @@ const ListHeader = styled.div`
 const ListTitle = styled.h1`
   font-family: Interop-Medium;
   font-size: 1.4rem;
+  line-height: var(--lineHeight-normal);
   line-height: var(--lineHeight-loose);
   margin: 0 1rem 0 0;
 `;
@@ -146,6 +111,7 @@ const ListSubTitle = styled.h3`
   font-family: Interop-Regular;
   color: var(--color-gray);
   font-size: 1.125rem;
+  line-height: var(--lineHeight-normal);
   margin: 0;
   ${media.lessThan("medium")`
     margin-bottom: 0.5rem;
@@ -160,32 +126,9 @@ const Gatherings = styled.div`
 `;
 
 const Home = () => {
+  const { gathList, filter } = useSelector(({ gathReducer }) => gathReducer);
   const dispatch = useDispatch();
   const history = useHistory();
-  const [list, setList] = useState({
-    sport: [],
-    area: [],
-    time: [
-      { id: 1, timeName: "오전" },
-      { id: 2, timeName: "오후" },
-      { id: 3, timeName: "저녁" },
-    ],
-  });
-  const [gathList, setGathList] = useState([]);
-  const [isSearched, setIsSearched] = useState(false);
-  const [sportInput, setSportInput] = useState("");
-  const [areaInput, setAreaInput] = useState("");
-  const [dateInput, setDateInput] = useState("");
-  const [timeInput, setTimeInput] = useState("");
-  const [totalNumInput, setTotalNumInput] = useState(null);
-  const [searchable, setSearchable] = useState(false);
-  const [conditions, setConditions] = useState({
-    sport: sportInput,
-    area: areaInput,
-    date: dateInput,
-    time: timeInput,
-    totalNum: totalNumInput,
-  });
 
   useEffect(() => {
     const checkValidUser = async () => {
@@ -204,88 +147,6 @@ const Home = () => {
     checkValidUser();
   }, [dispatch, history]);
 
-  useEffect(() => {
-    // 운동, 지역 리스트 받아오기
-    const getList = async () => {
-      try {
-        const sportList = await gathApi.getSportList();
-        const areaList = await gathApi.getAreaList();
-        setList({ ...list, sport: sportList.data, area: areaList.data });
-      } catch (err) {
-        // console.error(err);
-      }
-    };
-    getList();
-    // 모임 전체 리스트 받아오기
-    const getAllGathering = async () => {
-      try {
-        const res = await gathApi.getAllGath();
-        setGathList(res.data.gatherings);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getAllGathering();
-  }, []);
-
-  useEffect(() => {
-    if (sportInput && areaInput) {
-      setSearchable(true);
-    } else {
-      setSearchable(false);
-    }
-  }, [sportInput, areaInput]);
-
-  useEffect(() => {
-    setIsSearched(false);
-    const refinedSportInput = sportInput.match(/[A-Za-z가-힣]*/).join("");
-    const refinedDateInput = dateInput
-      .match(/[0-9]*/g)
-      .filter((el) => el.length > 0)
-      .join("-");
-
-    setConditions((prevState) => ({
-      ...prevState,
-      sport: refinedSportInput,
-      area: areaInput,
-      date: refinedDateInput,
-      time: timeInput,
-      totalNum: totalNumInput,
-    }));
-  }, [sportInput, areaInput, dateInput, timeInput, totalNumInput]);
-
-  useEffect(() => {
-    // 모임 검색
-    const findGathering = async () => {
-      try {
-        // 검색 조건 정제 (운동, 날짜)
-        const refinedSportInput = conditions.sport.match(/[A-Za-z가-힣]*/).join("");
-        const refinedDateInput = conditions.date
-          .match(/[0-9]*/g)
-          .filter((el) => el.length > 0)
-          .join("-");
-        const res = await gathApi.findGath({
-          ...conditions,
-          sport: refinedSportInput,
-          date: refinedDateInput,
-        });
-        setGathList(res.data.gatherings);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    if (isSearched) findGathering();
-  }, [isSearched]);
-
-  useEffect(() => {
-    dispatch(searchAction(conditions));
-  }, [conditions]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setIsSearched(true);
-  };
-
   const handleOnMapClick = () => {
     // TODO: 검색 실행 여부에 따른 별도의 조건으로 검색 및 지도에 표시 요청
   };
@@ -298,71 +159,26 @@ const Home = () => {
     <HomeContainer>
       <SearchContainer>
         <SearchTitle>💦 어떤 운동 모임을 찾으시나요? 🔍</SearchTitle>
-        <InputContainer onSubmit={handleSubmit}>
-          <InputList>
-            <SearchInput isSport name="운동" for="sport">
-              <InputDatalist
-                id="sport"
-                values={list.sport}
-                placeholder="어떤 운동하세요?"
-                item={sportInput}
-                setItem={setSportInput}
-              />
-            </SearchInput>
-            <SearchInput name="지역" for="area">
-              <InputDatalist
-                id="area"
-                values={list.area}
-                placeholder="지역 입력"
-                item={areaInput}
-                setItem={setAreaInput}
-              />
-            </SearchInput>
-            <SearchInput isDate name="날짜" for="date">
-              <InputDatepicker id="date" placeholder="날짜 입력" setDisplayedDate={setDateInput} />
-            </SearchInput>
-            <SearchInput isTime name="시간" for="time">
-              <InputDatalist
-                id="time"
-                values={list.time}
-                placeholder="시간 입력"
-                item={timeInput}
-                setItem={setTimeInput}
-              />
-            </SearchInput>
-            <SearchInput name="인원" for="totalNum">
-              <InputTotalNum
-                inputId="totalNum"
-                placeholder="인원 입력"
-                total={totalNumInput}
-                setTotal={setTotalNumInput}
-              />
-            </SearchInput>
-          </InputList>
-          <SearchBtnContainer>
-            {searchable && <SubmitInput type="submit" value="" />}
-            <SearchBtnView className="search-gathering" disabled={!searchable} />
-          </SearchBtnContainer>
-        </InputContainer>
+        <HomeSearchBar />
         <SearchTitle>💪🏻 운동 모임, 직접 만들어 보실래요? 🔥</SearchTitle>
         <Btn className="create-gathering" onClick={handleCreateGath}>
           모임 만들기
         </Btn>
       </SearchContainer>
       <ListContainer>
-        {conditions.sport && conditions.area ? (
+        {filter.sport && filter.area ? (
           <ListSubTitle>검색 결과</ListSubTitle>
         ) : (
           <ListSubTitle>스웻메이트에는 지금</ListSubTitle>
         )}
         <ListHeader>
-          {conditions.sport && conditions.area ? (
+          {filter.sport && filter.area ? (
             <ListTitle>
-              {conditions.date && `${conditions.date} `}
-              {conditions.time && `${conditions.time} `}
-              {`${conditions.area}의 `}
-              {conditions.totalNum && `${conditions.totalNum}인 `}
-              {`${conditions.sport} 모임`}
+              {filter.date && `${filter.date} `}
+              {filter.time && `${filter.time} `}
+              {`${filter.area}의 `}
+              {filter.totalNum && `${filter.totalNum}인 `}
+              {`${filter.sport} 모임`}
             </ListTitle>
           ) : (
             <ListTitle>이런 운동 모임들이 있어요!</ListTitle>
