2b971232a6a35ada8743d9a915aa0563fec5a329
ssumniee <suemink.dev@gmail.com>
2021-11-06 17:46:09 +0900

    [client] feat: #194 Update SearchInput & Input components' CSS to support responsive UI

--- client/src/components/InputDatalist.jsx
+++ client/src/components/InputDatalist.jsx
@@ -2,28 +2,26 @@ import React, { useMemo, useCallback } from "react";
 import styled from "styled-components";
 import DataListInput from "react-plain-datalist-input";
 import PropTypes from "prop-types";
+import media from "styled-media-query";
 import { IoCloseCircle } from "react-icons/io5";
 
 const Container = styled.div`
+  margin-top: 0.5rem;
   width: 100%;
-  .datalist-input {
-    padding-bottom: 0.75rem;
-  }
   .autocomplete-input {
     padding: 0 1rem;
+    width: 100%;
     ::placeholder {
       color: var(--color-gray);
       font-family: Interop-Light;
     }
     outline: none;
     font-size: 1rem;
-    margin-top: 0.25rem;
-    width: 100%;
   }
   .datalist-items {
     min-width: calc(100% + 4rem);
-    margin-top: 1.5rem;
     max-height: 15rem;
+    margin-top: 1.75rem;
     font-size: 1.125rem;
     background-color: var(--color-white);
     border-radius: 1rem;
@@ -32,14 +30,28 @@ const Container = styled.div`
     > * {
       padding: 1rem;
       :hover {
-        background-color: var(--color-maingreen--10);
+        ${media.greaterThan("medium")`
+          background-color: var(--color-maingreen--10);
+        `};
       }
     }
+    ${media.lessThan("medium")`
+      min-width: unset;
+      max-width: unset;
+      width: 100%;
+      margin-top: 1.25rem;
+      margin-bottom: -0.75rem;
+      max-height: 15rem;
+      filter: none;
+      border: 1px solid var(--color-maingreen--50);
+    `};
   }
   .datalist-active-item {
     background-color: var(--color-maingreen--25);
     :hover {
-      background-color: var(--color-maingreen--25);
+      ${media.greaterThan("medium")`
+        background-color: var(--color-maingreen--25);
+      `};
     }
   }
 `;
@@ -47,8 +59,9 @@ const Container = styled.div`
 const ClearBtn = styled.button`
   position: absolute;
   right: 1rem;
-  top: 50%;
-  transform: translateY(-50%);
+  top: 1.375rem;
+  /* top: 50%;
+  transform: translateY(-50%); */
   width: 1.25rem;
   height: 1.25rem;
   font-size: 1.25rem;
@@ -98,6 +111,7 @@ const HomeDatalist = ({ values, placeholder, item, setItem }) => {
         onSelect={onSelect}
         onInput={onInput}
         match={match}
+        suppressReselect={false}
       />
       {item && (
         <ClearBtn onClick={handleClearClick}>
