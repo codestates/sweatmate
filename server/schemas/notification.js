d47c4f6ee7344d0c1a64e48b020a791aff9fe452
YUN-KC <wjdrlfgg@gmail.com>
2021-11-07 13:07:18 +0900

    [server] chore: Modify notification fieldName gatheringId => room

--- server/schemas/notification.js
+++ server/schemas/notification.js
@@ -45,14 +45,14 @@ const notificationSchema = new Schema(
 
 notificationSchema.statics.createNotice = async function (userList, notificationInfo) {
   const { _id, room, type, url, target, message } = notificationInfo;
-  //타입 new와 gatheringId 에 대한 알림이 이미 있다면 알림이 추가되지 않는다.
+  //타입 new와 room 에 대한 알림이 이미 있다면 알림이 추가되지 않는다.
   if (type === "new") {
     let a = await this.updateMany(
       {
         _id: userList,
-        notification: { $not: { $elemMatch: { $and: [{ type: "new" }, { gatheringId: room }] } } },
+        notification: { $not: { $elemMatch: { $and: [{ type: "new" }, { room }] } } },
       },
-      { $push: { notification: { _id, gatheringId: room, type, url, target, message } } }
+      { $push: { notification: { _id, room, type, url, target, message } } }
     );
     console.log("뉴타입", a);
     return;
@@ -61,7 +61,7 @@ notificationSchema.statics.createNotice = async function (userList, notification
       {
         _id: userList,
       },
-      { $push: { notification: { _id, gatheringId: room, type, url, target, message } } }
+      { $push: { notification: { _id, room, type, url, target, message } } }
     );
     console.log("다른타입", a);
     return;
