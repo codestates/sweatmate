d714eefb5f28feb0e2096bad95a6bc7ad5aa25b2
ssumniee <suemink.dev@gmail.com>
2021-11-09 20:58:47 +0900

    [client] feat: #246 Update CSS of Landing to support mobile size devices

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import styled, { css } from "styled-components";
+import media from "styled-media-query";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import authApi from "../api/auth";
@@ -8,30 +9,60 @@ import Btn from "../components/Btn";
 
 const LandingContainer = styled.div`
   background-image: url(${(props) => props.url});
-  background-size: cover;
+  background-size: 150%;
+  background-position: top center;
   display: flex;
   flex-direction: column;
   overflow: auto;
   width: 100vw;
   height: fit-content;
   background-color: var(--color-white);
+  ${media.between("medium", "large")`
+    background-size: 200%;
+  `}
+  ${media.between("small", "medium")`
+    background-size: 300%;
+  `}
+  ${media.lessThan("small")`
+    background-size: 400%;
+  `}
 `;
 
 const CoverContainer = styled.div`
-  height: calc(100vh - 12rem);
+  height: 80vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  #btn-container {
+    margin: 0 auto;
+    padding: 2.5rem 1rem;
+    display: flex;
+    ${media.lessThan("small")`
+      width: 100%;
+      flex-direction: column;
+    `}
+  }
   .btn {
-    margin: 2.5rem auto;
-    border: 2px solid var(--color-maingreen--75);
+    border: 1px solid var(--color-maingreen--75);
     min-width: 15rem;
+    ${media.lessThan("small")`
+      width: 100%;
+      min-width: 20rem;
+      height: 3.2rem;
+    `}
     &.start,
     &.to-home {
       background-color: var(--color-maingreen--75);
       color: var(--color-white);
-      margin-right: 1rem;
+    }
+    &.start {
+      ${media.greaterThan("small")`
+        margin-right: 1rem;
+      `}
+      ${media.lessThan("small")`
+        margin-bottom: 1rem;
+      `}
     }
     &.experience {
       background-color: transparent;
@@ -50,6 +81,9 @@ const CoverTitle = styled.h1`
   font-weight: normal;
   font-family: Interop-SemiBold;
   font-size: 2rem;
+  ${media.lessThan("small")`
+    font-size: 1.75rem;
+  `}
 `;
 
 const CoverSubTitle = styled.h2`
@@ -58,15 +92,16 @@ const CoverSubTitle = styled.h2`
   font-weight: normal;
   font-family: Interop-Regular;
   font-size: 2rem;
+  ${media.lessThan("small")`
+    font-size: 1.5rem;
+  `}
 `;
 
 const KeyValueContainer = styled.div`
-  height: fit-content;
-  padding: 4rem 2rem;
-  display: flex;
-  justify-content: center;
   overflow: hidden;
   position: relative;
+  display: flex;
+  padding: 4rem 0;
   .fill {
     position: absolute;
     inset: 0;
@@ -81,29 +116,55 @@ const KeyValueContainer = styled.div`
             opacity: 0.75;
           `}
   }
+  .large {
+    flex: 3 0 2rem;
+    ${media.lessThan("small")`
+      flex: 3 0 1rem;
+    `}
+  }
+  .small {
+    flex: 1 0 2rem;
+    ${media.lessThan("small")`
+      flex: 1 0 1rem;
+    `}
+  }
   ${(props) =>
     props.align === "left" &&
     css`
       flex-direction: row-reverse;
+      .content {
+        flex-direction: row-reverse;
+      }
     `}
-  .desc {
-    ${(props) =>
-      props.align === "left"
-        ? css`
-            margin-right: 8rem;
-          `
-        : css`
-            margin-left: 8rem;
-          `}
-  }
+`;
+
+const ContentBox = styled.div`
+  height: fit-content;
+  display: flex;
+  ${media.lessThan("large")`
+    flex-direction: column !important;
+  `}
+  ${media.between("small", "medium")`
+    width: calc(100% - 4rem);
+  `}
+  ${media.lessThan("small")`
+    width: calc(100% - 2rem);
+  `}
 `;
 
 const DescrWrapper = styled.div`
-  /* border: 1px solid red; */
+  flex: 0 0 auto;
+  width: fit-content;
+  border: 1px solid red;
   display: flex;
   flex-direction: column;
   z-index: 1;
-  margin: auto 0;
+  ${media.between("small", "large")`
+    margin-bottom: 4rem;
+  `}
+  ${media.lessThan("small")`
+    margin-bottom: 2rem;
+  `}
 `;
 
 const DescTitle = styled.h2`
@@ -111,6 +172,12 @@ const DescTitle = styled.h2`
   font-weight: normal;
   font-family: Interop-Medium;
   font-size: 1.5rem;
+  ${media.between("small", "large")`
+    font-size: 1.375rem;
+  `}
+  ${media.lessThan("small")`
+    font-size: 1.25rem;
+  `}
   line-height: var(--lineHeight-loose);
 `;
 
@@ -120,6 +187,12 @@ const DescBody = styled.p`
   font-weight: normal;
   font-family: Interop-Light;
   font-size: 1.375rem;
+  ${media.between("small", "large")`
+    font-size: 1.25rem;
+  `}
+  ${media.lessThan("small")`
+    font-size: 1.125rem;
+  `}
   line-height: var(--lineHeight-loose);
 `;
 
@@ -129,56 +202,81 @@ const DescSubTitle = styled.h3`
   font-weight: normal;
   font-family: Interop-Medium;
   font-size: 1.25rem;
+  ${media.between("small", "large")`
+    font-size: 1.125rem;
+  `}
+  ${media.lessThan("small")`
+    font-size: 1rem;
+  `}
   line-height: var(--lineHeight-loose);
   color: var(--color-darkgray);
 `;
 
 const ImageWrapper = styled.div`
-  /* border: 1px solid blue; */
+  flex: 0 0 auto;
+  border: 1px solid blue;
   z-index: 1;
   position: relative;
   display: flex;
   &#first-container,
   &#third-container {
-    margin: 0 2rem 0 8rem;
+    margin-left: 4rem;
+    ${media.lessThan("large")`
+      margin-left: 0;
+    `}
   }
   &#second-container,
   &#fourth-container {
-    margin: 0 8rem 0 2rem;
+    margin-right: 4rem;
+    ${media.lessThan("large")`
+      margin-right: 0;
+    `}
   }
   &#first-container {
-    width: 38rem;
-    padding: 2.5rem 2rem;
+    align-items: flex-start;
   }
   &#second-container {
-    width: fit-content;
-    padding: 2rem;
+    width: 30rem;
+    max-width: 100%;
   }
   &#third-container {
-    width: fit-content;
-    padding: 6rem 2rem 2rem;
+    width: 38rem;
+    max-width: 100%;
+    flex-direction: column-reverse;
   }
   &#fourth-container {
-    padding: 2rem;
+    width: 34rem;
+    max-width: 100%;
   }
   .first {
     display: flex;
     flex-direction: column;
-    #sport-input {
-      width: 10rem;
-      margin-bottom: 0.5rem;
-    }
+    #sport-input,
     #sport-datalist {
       width: 10rem;
+      ${media.lessThan("small")`
+        width: 7.5rem;
+      `}
+    }
+    #sport-input {
+      margin-bottom: 0.5rem;
+    }
+    &.still {
+      margin-top: 2rem;
+      align-self: flex-start;
     }
     &.animation {
-      position: absolute;
-      top: 50%;
-      right: 0;
-      transform: translateY(-50%);
+      align-self: center;
+      margin-left: 4rem;
+      ${media.lessThan("small")`
+        margin-left: 2rem;
+      `}
     }
     .card {
       width: 20rem;
+      ${media.lessThan("small")`
+        width: 15rem;
+      `}
       filter: drop-shadow(2.5px 2.5px 5px var(--color-shadow));
       margin-bottom: 1rem;
       :last-of-type {
@@ -190,84 +288,88 @@ const ImageWrapper = styled.div`
     &.still {
       display: flex;
       flex-direction: column;
-      width: 30rem;
+      width: 100%;
     }
     &.animation {
       position: absolute;
       inset: 0;
     }
     .pin {
-      width: 3rem;
+      width: 10%;
       position: absolute;
       filter: drop-shadow(2.5px 2.5px 5px var(--color-shadow));
       &.one {
-        top: 4rem;
-        left: 23.5rem;
+        top: 7.5%;
+        left: 70%;
       }
       &.two {
-        top: 8.5rem;
-        left: 5.5rem;
+        top: 30%;
+        left: 16.7%;
       }
       &.three {
-        top: 16.25rem;
-        left: 16.5rem;
+        top: 70%;
+        left: 50%;
       }
     }
   }
   .third {
+    width: 100%;
+    max-width: 20rem;
     &.still {
+      align-self: flex-start;
       display: flex;
       flex-direction: column;
     }
     &.animation {
-      position: absolute;
-      inset: 0;
+      align-self: flex-end;
     }
     #upcoming {
-      width: 10rem;
-      height: 2.5rem;
-      margin-bottom: 1.5rem;
+      width: 50%;
+      margin-bottom: 1rem;
     }
     #upcoming-card {
-      width: 20rem;
+      width: 100%;
       filter: drop-shadow(2.5px 2.5px 5px var(--color-shadow));
     }
     #alarm {
-      width: 20rem;
+      width: 100%;
       filter: drop-shadow(2.5px 2.5px 5px var(--color-shadow));
-      position: absolute;
-      left: 14rem;
-      top: 0rem;
+      margin-bottom: -2rem;
+      ${media.lessThan("medium")`
+        margin-bottom: 1rem;
+      `}
     }
   }
   .fourth {
     &.animation {
       display: flex;
       flex-direction: column;
-      width: 28rem;
-      height: calc(41px + 64px + 64px + 4rem);
-      position: relative;
+      height: fit-content;
+      width: 100%;
     }
     .chat {
-      position: absolute;
       filter: drop-shadow(2.5px 2.5px 5px var(--color-shadow));
+      margin-bottom: 2rem;
+      ${media.between("small", "large")`
+        margin-bottom: 1.5rem;
+      `}
+      ${media.lessThan("small")`
+        margin-bottom: 1rem;
+      `}
+      align-self: flex-start;
       &.one {
-        width: 272px;
-        height: 41px;
-        left: 0;
-        top: 0;
+        width: calc(80% - 2.5rem);
+        max-width: 272px;
       }
       &.two {
-        right: 0;
-        top: calc(41px + 2rem);
-        width: 338px;
-        height: 64px;
+        width: calc(99.4% - 2.5rem);
+        max-width: 338px;
+        align-self: flex-end;
       }
       &.three {
-        width: 340px;
-        height: 64px;
-        left: 0;
-        top: calc(41px + 64px + 4rem);
+        width: calc(100% - 2.5rem);
+        max-width: 340px;
+        margin-bottom: 0;
       }
     }
   }
@@ -348,10 +450,10 @@ const Landing = () => {
           ) : (
             <>
               <Btn className="start btn" onClick={handleStartClick}>
-                스웻메이트 시작하기
+                시작하기
               </Btn>
               <Btn className="experience btn" onClick={handleExperienceClick}>
-                일단 체험해볼래요
+                체험해보기
               </Btn>
             </>
           )}
@@ -359,95 +461,111 @@ const Landing = () => {
       </CoverContainer>
       <KeyValueContainer colored align="right">
         <div className="fill" />
-        <DescrWrapper className="desc">
-          <DescTitle>
-            쉽고 간편한
-            <br />
-            운동 메이트 찾기
-          </DescTitle>
-          <DescBody>
-            다양한 모임 필터를 통해
-            <br />
-            나와 딱 맞는 운동 메이트를 찾아보세요
-          </DescBody>
-          <DescSubTitle>모임 검색 & 모임 만들기</DescSubTitle>
-        </DescrWrapper>
-        <ImageWrapper id="first-container">
-          <div className="first still">
-            <img id="sport-input" src="../images/sport-input.png" />
-            <img id="sport-datalist" src="../images/sport-datalist.png" />
-          </div>
-          <div className="first animation">
-            <img className="card top" src="../images/card-1-1.png" />
-            <img className="card mid" src="../images/card-1-2.png" />
-            <img className="card btm" src="../images/card-1-3.png" />
-          </div>
-        </ImageWrapper>
+        <div className="large margin" />
+        <ContentBox className="content">
+          <DescrWrapper className="desc">
+            <DescTitle>
+              쉽고 간편한
+              <br />
+              운동 메이트 찾기
+            </DescTitle>
+            <DescBody>
+              다양한 모임 필터를 통해
+              <br />
+              나와 딱 맞는 운동 메이트를 찾아보세요
+            </DescBody>
+            <DescSubTitle>모임 검색 & 모임 만들기</DescSubTitle>
+          </DescrWrapper>
+          <ImageWrapper id="first-container">
+            <div className="first still">
+              <img id="sport-input" src="../images/sport-input.png" />
+              <img id="sport-datalist" src="../images/sport-datalist.png" />
+            </div>
+            <div className="first animation">
+              <img className="card top" src="../images/card-1-1.png" />
+              <img className="card mid" src="../images/card-1-2.png" />
+              <img className="card btm" src="../images/card-1-3.png" />
+            </div>
+          </ImageWrapper>
+        </ContentBox>
+        <div className="small margin" />
       </KeyValueContainer>
       <KeyValueContainer align="left">
         <div className="fill" />
-        <DescrWrapper className="desc">
-          <DescTitle>
-            지도에서 찾는
-            <br />내 주변의 운동 모임
-          </DescTitle>
-          <DescBody>
-            내 주변 운동 모임,
-            <br />
-            지도 위에서 바로 확인해 보세요
-          </DescBody>
-          <DescSubTitle>지도로 보기</DescSubTitle>
-        </DescrWrapper>
-        <ImageWrapper id="second-container">
-          <div className="second still">
-            <img id="map-bg" src="../images/map-bg.png" />
-          </div>
-          <div className="second animation">
-            <img className="pin one" src="../images/map-pin-1.png" />
-            <img className="pin two" src="../images/map-pin-2.png" />
-            <img className="pin three" src="../images/map-pin-3.png" />
-          </div>
-        </ImageWrapper>
+        <div className="large margin" />
+        <ContentBox className="content">
+          <DescrWrapper className="desc">
+            <DescTitle>
+              지도에서 찾는
+              <br />내 주변의 운동 모임
+            </DescTitle>
+            <DescBody>
+              내 주변 운동 모임,
+              <br />
+              지도 위에서 바로 확인해 보세요
+            </DescBody>
+            <DescSubTitle>지도로 보기</DescSubTitle>
+          </DescrWrapper>
+          <ImageWrapper id="second-container">
+            <div className="second still">
+              <img id="map-bg" src="../images/map-bg.png" />
+            </div>
+            <div className="second animation">
+              <img className="pin one" src="../images/map-pin-1.png" />
+              <img className="pin two" src="../images/map-pin-2.png" />
+              <img className="pin three" src="../images/map-pin-3.png" />
+            </div>
+          </ImageWrapper>
+        </ContentBox>
+        <div className="small margin" />
       </KeyValueContainer>
       <KeyValueContainer colored align="right">
         <div className="fill" />
-        <DescrWrapper className="desc">
-          <DescTitle>쉬운 일정 관리</DescTitle>
-          <DescBody>
-            예정된 모임 일정을 모아 보고
-            <br />
-            리마인더 알림도 받아보세요
-          </DescBody>
-          <DescSubTitle>모임 일정 & 실시간 알림</DescSubTitle>
-        </DescrWrapper>
-        <ImageWrapper id="third-container">
-          <div className="third still">
-            <img id="upcoming" src="../images/upcoming.png" />
-            <img id="upcoming-card" src="../images/card-3.png" />
-          </div>
-          <div className="third animation">
-            <img id="alarm" src="../images/alarm.png" />
-          </div>
-        </ImageWrapper>
+        <div className="large margin" />
+        <ContentBox className="content">
+          <DescrWrapper className="desc">
+            <DescTitle>쉬운 일정 관리</DescTitle>
+            <DescBody>
+              예정된 모임 일정을 모아 보고
+              <br />
+              리마인더 알림도 받아보세요
+            </DescBody>
+            <DescSubTitle>모임 일정 & 실시간 알림</DescSubTitle>
+          </DescrWrapper>
+          <ImageWrapper id="third-container">
+            <div className="third still">
+              <img id="upcoming" src="../images/upcoming.png" />
+              <img id="upcoming-card" src="../images/card-3.png" />
+            </div>
+            <div className="third animation">
+              <img id="alarm" src="../images/alarm.png" />
+            </div>
+          </ImageWrapper>
+        </ContentBox>
+        <div className="small margin" />
       </KeyValueContainer>
       <KeyValueContainer align="left">
         <div className="fill" />
-        <DescrWrapper className="desc">
-          <DescTitle>부담없는 소통</DescTitle>
-          <DescBody>
-            모임에 참여하는 사람들끼리
-            <br />
-            바로 소통할 수 있어요
-          </DescBody>
-          <DescSubTitle>모임 채팅</DescSubTitle>
-        </DescrWrapper>
-        <ImageWrapper id="fourth-container">
-          <div className="fourth animation">
-            <img className="chat one" src="../images/chat-1-colored.png" />
-            <img className="chat two" src="../images/chat-2-colored.png" />
-            <img className="chat three" src="../images/chat-3-colored.png" />
-          </div>
-        </ImageWrapper>
+        <div className="large margin" />
+        <ContentBox className="content">
+          <DescrWrapper className="desc">
+            <DescTitle>부담없는 소통</DescTitle>
+            <DescBody>
+              모임에 참여하는 사람들끼리
+              <br />
+              바로 소통할 수 있어요
+            </DescBody>
+            <DescSubTitle>모임 채팅</DescSubTitle>
+          </DescrWrapper>
+          <ImageWrapper id="fourth-container">
+            <div className="fourth animation">
+              <img className="chat one" src="../images/chat-1-colored.png" />
+              <img className="chat two" src="../images/chat-2-colored.png" />
+              <img className="chat three" src="../images/chat-3-colored.png" />
+            </div>
+          </ImageWrapper>
+        </ContentBox>
+        <div className="small margin" />
       </KeyValueContainer>
     </LandingContainer>
   );
