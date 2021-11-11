3c520c54da348923dc31fd9daa2f2cc31c5dfd97
ssumniee <suemink.dev@gmail.com>
2021-11-11 21:13:37 +0900

    [client] feat: #252 Update Notification component

--- client/src/components/Notification.jsx
+++ client/src/components/Notification.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled, { css } from "styled-components";
+import { Link } from "react-router-dom";
 import { IoChevronForward, IoClose } from "react-icons/io5";
+import media from "styled-media-query";
 
 const NotiContainer = styled.div`
+  width: 24rem;
+  ${media.lessThan("small")`
+    width: calc(100vw - 2rem);
+    max-width: 24rem;
+  `};
   display: flex;
   align-items: center;
   padding: 0.5rem 0.75rem;
@@ -15,6 +22,17 @@ const NotiContainer = styled.div`
   }
   cursor: pointer;
   ${(props) =>
+    props.disabled &&
+    css`
+      cursor: unset;
+      #go {
+        visibility: hidden !important;
+      }
+      :hover {
+        background-color: var(--color-darkwhite);
+      }
+    `}
+  ${(props) =>
     props.isEmpty &&
     css`
       cursor: unset;
@@ -23,9 +41,18 @@ const NotiContainer = styled.div`
       }
       #go,
       #delete {
-        visibility: hidden !important;
+        visibility: hidden;
       }
     `}
+  position: relative;
+`;
+
+const GoToLink = styled(Link)`
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
 `;
 
 const NotiContent = styled.div`
@@ -49,8 +76,6 @@ const NotiMessage = styled.p`
   font-size: 0.875rem;
   line-height: var(--lineHeight-loose);
   font-family: Interop-Regular;
-  max-width: 20rem;
-  min-width: 15rem;
   word-wrap: break-word;
   word-break: keep-all;
   ${(props) =>
@@ -72,6 +97,7 @@ const GoMark = styled.div`
   :hover {
     color: var(--color-maingreen--100);
   }
+  z-index: 1;
 `;
 
 const DeleteBtn = styled.button`
@@ -85,19 +111,21 @@ const DeleteBtn = styled.button`
     color: var(--color-red);
     opacity: 0.9;
   }
+  z-index: 1;
 `;
 
-function Notification({ isEmpty, item }) {
+function Notification({ isEmpty, item, handleNotiClick, handleDeleteClick }) {
   return (
-    <NotiContainer isEmpty={isEmpty}>
-      <NotiContent onClick={() => {}}>
+    <NotiContainer isEmpty={isEmpty} disabled={!item.url}>
+      {item.url && <GoToLink to={item.url} onClick={handleNotiClick} />}
+      <NotiContent>
         {item.title && <NotiTitle>{item.title}</NotiTitle>}
         <NotiMessage disabled={isEmpty}>{item.message}</NotiMessage>
       </NotiContent>
       <GoMark id="go">
         <IoChevronForward />
       </GoMark>
-      <DeleteBtn id="delete" onClick={() => {}}>
+      <DeleteBtn id="delete" onClick={handleDeleteClick}>
         <IoClose />
       </DeleteBtn>
     </NotiContainer>
@@ -125,40 +153,9 @@ Notification.propTypes = {
     title: PropTypes.string,
     type: PropTypes.string,
     url: PropTypes.string,
-  }),
+  }).isRequired,
+  handleNotiClick: PropTypes.func,
+  handleDeleteClick: PropTypes.func,
 };
 
 export default Notification;
-
-/* 
-<NotificationWrapper>
-  {notificationList.length === 0 && (
-    <NotificationItem>
-      <Message>메시지가 없습니다.</Message>
-    </NotificationItem>
-  )}
-  {notificationList.map((item) => (
-    <NotificationItem key={item.id}>
-      <Message>
-        {item.title}
-        <br />
-        {item.message}
-      </Message>
-      {item.url && (
-        <GoLink
-          to={item.url}
-          onClick={() => {
-            closeAll();
-            handleNotiBtnClick(item.id);
-          }}
-        >
-          <IoChevronForward />
-        </GoLink>
-      )}
-      <DeleteBtn type="button" onClick={() => handleNotiBtnClick(item.id)}>
-        <IoClose />
-      </DeleteBtn>
-    </NotificationItem>
-  ))}
-</NotificationWrapper>; 
-*/
