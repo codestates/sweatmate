5211592e63fd899b85d7aaa6a630a8c0de4d6c30
heegu0311 <heegu0311@gmail.com>
2021-12-16 00:23:10 +0900

    [client] fix: #331 Update date argument of parseISO to eliminate warning

--- client/src/components/GathCard.jsx
+++ client/src/components/GathCard.jsx
@@ -5,7 +5,7 @@ import UserProfile from "./UserProfile";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { gathDetailModalOnAction } from "../store/actions";
-import { getMonth, getDate, parseISO } from "date-fns";
+import { getMonth, getDate } from "date-fns";
 
 const CardContainer = styled.div`
   border-radius: 1rem;
@@ -96,8 +96,8 @@ const GathCard = ({ gathering, ...rest }) => {
     <CardContainer onClick={handleGathDetailModalOn} {...rest}>
       <InfoHeader>
         <div>
-          <div>{`${getMonth(parseISO(gathering.date)) + 1 || "00"}월 ${
-            getDate(parseISO(gathering.date)) || "00"
+          <div>{`${getMonth(new Date(gathering.date)) + 1 || "00"}월 ${
+            getDate(new Date(gathering.date)) || "00"
           }일`}</div>
           <div className="divider">|</div>
           <div>{gathering.time}</div>
@@ -137,7 +137,7 @@ GathCard.propTypes = {
     placeName: PropTypes.string,
     latitude: PropTypes.string,
     longitude: PropTypes.string,
-    date: PropTypes.string,
+    date: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
     time: PropTypes.string,
     timeDescription: PropTypes.string,
     totalNum: PropTypes.number,
